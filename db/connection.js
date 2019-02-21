1f26a5bb3f8963ad10095d50d2ff1e768ab359a2
Shalain DeRamus <shalainderamus@PSADMINs-MacBook-Air-55.local>
2019-02-21 15:21:42 -0500

    setup express server

---
+++ db/connection.js
@@ -0,0 +1,8 @@
+require('dotenv').config(); const mongoose = require("mongoose")
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true }).then(() => {
+
+console.log("MONGODB is now connected")
+})
+
+module.exports = mongoose;
