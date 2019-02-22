6e8c388a5e6666ab6fa599159d7c1dbee5192531
Shalain DeRamus <shalainderamus@PSADMINs-MacBook-Air-55.local>
2019-02-22 15:06:21 -0500

    added react routes

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,10 @@ import Signup from './components/User/Signup';
 import Welcome from './components/Walkthrough/Welcome';
 import SelectCat from './components/Walkthrough/SelectCat';
 import NewCat from './components/Categories/AddCat';
+import AddStashItem from './components/StashItems/AddStashItem';
+import ViewCats from './components/Categories/ViewCats';
+import StashItemList from './components/StashItems/StashItemList';
+import Profile from './components/User/Profile';
 
 
 
@@ -16,21 +20,19 @@ class App extends Component {
     return (
       <div>
          <Router>
-        <div>      
-          <Switch>
-            <Route exact path="/" component={Intro} />
-            <Route exact path="/sign-up" component={Signup} />
-            <Route exact path="/welcome" component={Welcome} />
-            <Route exact path="/select-categories" component={SelectCat} />
-            <Route exact path="/add-category" component={NewCat} />
-            <Route exact path="/merch/astro-denim-jacket" component={AstroDenimJacket} />
-            <Route exact path="/merch/astro-tie-dye-tshirt" component={AstroTieDye} />
-            <Route exact path="/merch/thrills-and-chills-hoodie" component={ThrillsAndChillsHoodie} />
-            <Route exact path="/merch/thrills-and-chills-pants" component={ThrillsAndChillsPants} />
-
-            <Route exact path="/listening-party" component={ListeningParty} />
-          </Switch>
-        </div>
+              <div>      
+                <Switch>
+                  <Route exact path="/" component={Intro} />
+                  <Route exact path="/sign-up" component={Signup} />
+                  <Route exact path="/welcome" component={Welcome} />
+                  <Route exact path="/select-categories" component={SelectCat} />
+                  <Route exact path="/add-category" component={NewCat} />
+                  <Route exact path="/view-categories" component={ViewCats} />
+                  <Route exact path="/add-stash-item" component={AddStashItem} />
+                  <Route exact path="/stash-item-list" component={StashItemList} />
+                  <Route exact path="/profile" component={Profile} />
+                </Switch>
+              </div>
       </Router>
       </div>
     );
